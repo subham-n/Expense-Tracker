40a3003b162632666c0322c08174ee769fa673b7
subham-n <subhamnandi2002@gmail.com>
2024-07-14 16:58:52 +0530

    codes

---
+++ config/connectDb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server Running On ${mongoose.connection.host}`);
+  } catch (error) {
+    console.log(`${error}`);
+  }
+};
+
+module.exports = connectDb;
